429822dda6baf16d544c69ea134377312c2beb98
Cgalvispadilla <fktcg99@gmail.com>
2021-09-19 14:32:46 -0500

    se creo la entidad todo

---
+++ Backend/src/test/java/com/sofkau/crudtodo/CrudTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.crudtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
