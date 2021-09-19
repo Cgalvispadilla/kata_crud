0585c4c4b133f7710a421bac501b302d2a21c551
Cgalvispadilla <fktcg99@gmail.com>
2021-09-19 15:11:11 -0500

    se agrego el controlador del todo

--- Backend/src/main/java/com/sofkau/crudtodo/entities/Todo.java
+++ Backend/src/main/java/com/sofkau/crudtodo/entities/Todo.java
@@ -12,7 +12,7 @@ import javax.persistence.Id;
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String name;
     private boolean completed;
 }
